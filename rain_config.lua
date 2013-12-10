5ec6cd2e465c23a46ad132c55607baa1515548ce
Rainrider <rainrider.wow@gmail.com>
2013-12-10 21:55:35 +0100

    config: make cfg a frame to enable event tracking

--- rain_config.lua
+++ rain_config.lua
@@ -4,72 +4,72 @@
 	==========================--]]
 local _, ns = ...
 
-ns.cfg = {
+local cfg = CreateFrame("Frame", "raincfg")
+
 -- frame visibility
-	-- true to show; false to hide
-	showParty = true,
-	showPartyTargets = true,
-	showPartyPets = true,
-	showRaid = true,
-	showMT = true,					-- maintanks
-	showMTT = true,					-- maintanks' targets
+-- true to show; false to hide
+cfg.showParty = true
+cfg.showPartyTargets = true
+cfg.showPartyPets = true
+cfg.showRaid = true
+cfg.showMT = true					-- maintanks
+cfg.showMTT = true					-- maintanks' targets
 
 -- layout
-	horizParty = true,				-- true for horizontal party layout; false for vertical
+cfg.horizParty = true				-- true for horizontal party layout; false for vertical
 
--- auras
-	-- filtering
-	showPlayerBuffs = true,			-- true to enable the display of player buffs left to the player frame; false to disable them
-	onlyShowPlayerBuffs = false,		-- true to show only player buffs; false to show all buffs (for friendly targets only)
-	onlyShowPlayerDebuffs = false,	-- true to show only player debuffs; false to show only player class debuffs (for enemies only)
+-- auras filtering
+cfg.showPlayerBuffs = true			-- true to enable the display of player buffs left to the player frame; false to disable them
+cfg.onlyShowPlayerBuffs = false		-- true to show only player buffs; false to show all buffs (for friendly targets only)
+cfg.onlyShowPlayerDebuffs = false	-- true to show only player debuffs; false to show only player class debuffs (for enemies only)
 
-	-- debuff highlightingt
-	dispelTypeFilter = true,		-- true to highlight only debuffs the player can dispel; false to only highlight boss debuffs
+-- debuff highlightingt
+cfg.dispelTypeFilter = true 		-- true to highlight only debuffs the player can dispel; false to only highlight boss debuffs
 
 -- class specific
-	-- A value of 1 behind the spell means the buff/debuff should be applied by the player in order to be shown
-	-- A maximum of 3 buffs and 3 debuffs per class allowed
-	buffTable = {
-		["DRUID"] = {
-			[33763] = 1,	-- Life Bloom
-		},
-		["PRIEST"] = {
-			[139] = 1,		-- Renew
-			[33076] = 1,	-- Prayer of Mending
-		},
-		["SHAMAN"] = {
-			[974] = 2,		-- Earth Shield
-		},
+-- A value of 1 behind the spell means the buff/debuff should be applied by the player in order to be shown
+-- A maximum of 3 buffs and 3 debuffs per class allowed
+cfg.buffTable = {
+	["DRUID"] = {
+		[33763] = 1,	-- Life Bloom
 	},
+	["PRIEST"] = {
+		[139] = 1,		-- Renew
+		[33076] = 1,	-- Prayer of Mending
+	},
+	["SHAMAN"] = {
+		[974] = 2,		-- Earth Shield
+	},
+}
 
-	debuffTable = {
-		["PALADIN"] = {
-			[25771] = 2,	-- Forbearance
-		},
-		["PRIEST"] = {
-			[6788] = 2,		-- Weakened Soul
-		},
+cfg.debuffTable = {
+	["PALADIN"] = {
+		[25771] = 2,	-- Forbearance
 	},
+	["PRIEST"] = {
+		[6788] = 2,		-- Weakened Soul
+	},
+}
 
 -- click casting spell
-	-- use i.e. ["ENGLISH_CLASS_NAME"] = spellID,
-	clickSpell = {
-		["DEATHKNIGHT"] = 61999,-- Raise Ally (61999)
-		["DRUID"] = 29166,		-- Innervate (29166)
-		["HUNTER"] = 34477,		-- Misdirection (34477)
-		["MAGE"] = 475,			-- Remove Curse (475)
-		["MONK"] = 115450,		-- Detox (115450)
-		["PALADIN"] = 31789,	-- Righteous Defense (31789)
-		["PRIEST"] = 73325,		-- Leap of Faith (73325)
-		["ROGUE"] = 57934,		-- Tricks of the Trade (57934)
-		["SHAMAN"] = 546,		-- Water Walking (546)
-		["WARRIOR"] = 3411,		-- Intervene (3411)
-		["WARLOCK"] = 109773,	-- Dark Intent (109773)
-	},
+-- use i.e. ["ENGLISH_CLASS_NAME"] = spellID,
+cfg.clickSpell = {
+	["DEATHKNIGHT"] = 61999,	-- Raise Ally (61999)
+	["DRUID"] = 29166,			-- Innervate (29166)
+	["HUNTER"] = 34477,			-- Misdirection (34477)
+	["MAGE"] = 475,				-- Remove Curse (475)
+	["MONK"] = 115450,			-- Detox (115450)
+	["PALADIN"] = 31789,		-- Righteous Defense (31789)
+	["PRIEST"] = 73325,			-- Leap of Faith (73325)
+	["ROGUE"] = 57934,			-- Tricks of the Trade (57934)
+	["SHAMAN"] = 546,			-- Water Walking (546)
+	["WARRIOR"] = 3411,			-- Intervene (3411)
+	["WARLOCK"] = 109773,		-- Dark Intent (109773)
+}
 
 -- other
-	raidHealth = 1,					-- 0 - none; 1 - deficit; 2 - percent
-}
+cfg.raidHealth = 1 			-- 0 - none; 1 - deficit; 2 - percent
+
 
 ns.media = {
 	FONT = [=[Interface\AddOns\oUF_Rain\media\fonts\russel square lt.ttf]=],
@@ -101,3 +101,14 @@ ns.media = {
 
 -- do not touch anything below
 ns.playerClass = select(2, UnitClass("player"))
+ns.playerSpec = GetSpecialization() or 0
+
+cfg:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
+cfg:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+
+function cfg:PLAYER_SPECIALIZATION_CHANGED(unit)
+	if (unit ~= "player") then return end
+	ns.playerSpec = GetSpecialization() or 0
+end
+
+ns.cfg = cfg
