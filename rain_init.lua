fea6987a59f218542a5625a86d20a6d87833818c
Rainrider <rainrider.wow@gmail.com>
2014-07-06 00:21:55 +0200

    use AdiDebug

--- rain_init.lua
+++ rain_init.lua
@@ -4,6 +4,12 @@ local frame = CreateFrame("Frame")
 frame:SetScript("OnEvent", function(self, event, ...) if self[event] then self[event](self, ...) end end)
 frame:RegisterEvent("ADDON_LOADED")
 
+if (AdiDebug) then
+	ns.Debug = AdiDebug:Embed({}, addon)
+else
+	ns.Debug = function() end
+end
+
 function frame:ADDON_LOADED(name)
 	if name ~= addon then return end
 
